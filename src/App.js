cb827b93743e887605c5cd2490b64b8187c6eb3c
Adrian Widmer <74932333+Awi-24@users.noreply.github.com>
2022-07-25 02:02:05 -0300

    V1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './app.scss';
 import Navbar from './components/Navbar/Navbar'
+import Main from './pages/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,24 @@ import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div className='container-navbar'>
-      
+    <div className='container-navbar sticky-top'>
+        <div className='navbar navbar-expand justify-content-center'>
+
+          <ul className='navbar-nav text-uppercase mr-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>Sobre Mim</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>Habilidades</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>Serviços</a>
+            </li>
+            <li>
+              <a className='nav-link' href='/'>Projetos</a>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
